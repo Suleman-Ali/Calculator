cd14b4ca1b59c91fa32a705b3bb06f40b83bcaa8
Suleman <115074330+Suleman-Ali@users.noreply.github.com>
2022-12-01 16:40:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -73,6 +73,6 @@ async function StartAgain() {
             message: "Do you want to continue ? Press (y/n):"
         });
     } while (again.restart == 'y' || again.restart == 'Y' || again.restart == "yes" || again.restart == 'Yes' || again.restart == 'YES');
-    console.log("\n Thank You For Using Calculator.");
+    console.log("\n Thank You For Using Calculation.");
 }
 StartAgain();
